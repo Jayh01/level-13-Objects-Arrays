25ab96fc390f02631fdc0fba33e25904a15b2377
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-08-08 15:35:41 +0200

    Add files via upload

---
+++ level-13-Objects-Arrays/index.js
@@ -0,0 +1,75 @@
+const person = {
+    name: 'jaap',
+    age : 18    ,
+    evaluations: [7, 10, 9],
+    }  
+        console.log(person.name);
+        console.log(person['age'])
+        console.log(person.evaluations) //De waarde zijn 7, 10, 9,
+    
+const kleurcode = ["groen", "blauw", "rood"]    
+        console.log(kleurcode.length);
+        console.log(kleurcode[0])
+        console.log(kleurcode[2])
+        const kleurcodeExtra = kleurcode.push('geel') ;
+        const kleurcodenum = kleurcode.push(5);        
+                console.log(kleurcode)
+        const text = kleurcode.push ({greeting: "hi ik ben een object"});
+        console.log(kleurcode[5])
+
+// laatste opdracht
+
+const catBreeds = [{
+        name: "Abyssinian",
+        description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+        dog_friendly: 4,
+        energy_level: 5,
+        life_span: "14 - 15",
+        origin: "Egypt",
+        temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+        wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+                    food: {
+                            favourite_food: "fish",
+          medium_liked_food: "dried fruits", 
+          disliked_food: "walnuts"
+         }
+      },
+            {
+        name: "Aegean",
+        description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+        dog_friendly: 4,
+        energy_level: 53,
+        life_span: "9- 12",
+        origin: "Greece",
+        temperament: [ "Affectionate", "Social", "Intelligent", "Playful", "Active"],
+        wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+        food: {
+                            favourite_food: "tuna",
+          medium_liked_food: "canned food", 
+          disliked_food: "all fruits"
+         }
+      },
+            {
+        name: "American Bobtail",
+        description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+        dog_friendly: 5,
+        energy_level: 3,
+        life_span: "11 - 15",
+        origin: "United States",
+        temperament: [ "Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+        wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+        food: {
+                            favourite_food: "meaty things",
+          medium_liked_food: "tuna", 
+          disliked_food: "canned food"
+         }
+      }
+    ]
+                console.log(catBreeds)
+                console.log('kat 3',catBreeds[2], "American Bobtail")
+                console.log('kat 1',catBreeds[0],)
+
+
+
+                
+              
